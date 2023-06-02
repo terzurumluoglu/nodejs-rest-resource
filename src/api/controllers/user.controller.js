aa962a3ff586258d72c5447113d0550e6dc3682d
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-06-03 00:16:38 +0300

    update user controller

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -3,17 +3,17 @@ const { getAllUser, getUserById } = require('../services/user.service');
 // @desc   Get All Users
 // @route  GET /users
 // @access Private
-exports.getAllUsers = (req, res, next) => {
-    const users = getAllUser();
+exports.getAllUsers = async (req, res, next) => {
+    const users = await getAllUser();
     res.status(200).send(users);
 };
 
 // @desc   Get UserById
 // @route  GET /users/:id
 // @access Private
-exports.getUserById = (req, res, next) => {
-    const { id } = req.params;
-    const user = getUserById(+id);
+exports.getUserById = async (req, res, next) => {
+    const { email } = req.params;
+    const user = await getUserById(email);
     if (!user) {
         res.status(404).send('Not Found');
     }
