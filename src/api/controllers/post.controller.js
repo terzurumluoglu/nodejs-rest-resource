e7f5ae4208e6fcda0e1bfc271bc2e892558a4011
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-07-06 21:24:09 +0300

    renamed getAllPosts and added findOne, save and deleteOne

--- src/api/controllers/post.controller.js
+++ src/api/controllers/post.controller.js
@@ -1,9 +1,41 @@
-const { getAllPosts } = require('../services/post.service');
+const { find, findOne, save, deleteOne } = require('../services/post.service');
+const { slugify } = require('../utils/slugify');
+const { convertIdToObjectId } = require('../utils/utils');
 
 // @desc   Get All Posts
 // @route  GET /posts
 // @access Public
-exports.getAllPosts = async (req, res, next) => {
-    const posts = await getAllPosts();
+exports.find = async (req, res, next) => {
+    const posts = await find();
     res.status(200).send(posts);
+};
+
+// @desc   Get Posts By Filter
+// @route  GET /post(s)
+// @access Public
+exports.findOne = async (req, res, next) => {
+    let { params } = req;
+    params = convertIdToObjectId(params);
+    const posts = await findOne(params);
+    res.status(200).send(posts);
+};
+
+// @desc   Create a New Post
+// @route  POST /posts
+// @access Public
+exports.save = async (req, res, next) => {
+    const { title, content } = req.body;
+    const slug = slugify(title);
+    const data = await save({ title, content, slug });
+    res.status(200).send(data);
+};
+
+// @desc   Delete a Post
+// @route  DELETE /posts/:_id
+// @access Public
+exports.deleteOne = async (req, res, next) => {
+    let { params } = req;
+    params = convertIdToObjectId(params);
+    const data = await deleteOne(params);
+    res.status(200).send(data);
 };
