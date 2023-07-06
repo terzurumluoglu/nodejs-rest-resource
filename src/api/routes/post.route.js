bf0ac7efff76a40898b925d04e38eb30b1d4845a
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-07-06 21:24:22 +0300

    post route changed

--- src/api/routes/post.route.js
+++ src/api/routes/post.route.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getAllPosts } = require('../controllers/post.controller');
+const { deleteOne, find, findOne, save } = require('../controllers/post.controller');
 
-router.route('/').get(getAllPosts);
+router.route('/').get(find).post(save);
+
+router.route('/:_id').get(findOne).delete(deleteOne);
 
 module.exports = router;
