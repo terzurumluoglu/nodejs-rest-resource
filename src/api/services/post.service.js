aaf539d9f258bb087c96fb1daba03f5198d5b7ed
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-07-08 13:06:07 +0300

    added comment

--- src/api/services/post.service.js
+++ src/api/services/post.service.js
@@ -5,20 +5,52 @@ const collection = getDatabase().collection(collections.posts);
 
 class PostService {
 
+    /**
+     * find
+     * @param query
+     * @returns Posts as Array
+     */
     find = (query) => collection.find(query).toArray();
     
+    /**
+     * findOne
+     * @param param 
+     * @returns Post as Object
+     */
     findOne = (param) => collection.findOne(param);
     
+    /**
+     * save
+     * @param post
+     * @returns MongoDB Response
+     */
     save = async (post) => {
         const slug = await this.#generateSlug(post.title);
         post = { ...post, slug, slugCount: 0 };
         return collection.insertOne(post);
     };
 
+    /**
+     * deleteOne
+     * @param param 
+     * @returns MongoDB Response
+     */
     deleteOne = (param) => collection.deleteOne(param);
 
+    /**
+     * updateOne
+     * @param param
+     * @param values
+     * @returns MongoDB Response
+     */
     updateOne = (param, values) => collection.updateOne(param, values);
 
+    /**
+     * #generateSlug
+     * * private
+     * @param title 
+     * @returns string
+     */
     #generateSlug = async (title) => {
         const slug = slugify(title);
         const post = await this.findOne({slug});
